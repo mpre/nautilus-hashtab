40a403be5dc8d854cc501a97967c4ffeb2268dde
Marco Previtali <prex@hotmail.it>
2010-03-26 13:36:10 +0100

    Add settings' window

--- hashtab.py
+++ hashtab.py
@@ -28,10 +28,10 @@ class HashTab(nautilus.PropertyPageProvider):
         tab_label = gtk.Label('HashTab')
         tab_label.show()
 
-        self.HASH_TYPES = ('MD5', 'SHA1', 'SHA512')
+        self.HASH_TYPES = ('MD5', 'SHA1', 'SHA256', 'SHA512')
 
         # Settings dict
-        type_conf = self.load_conf()
+        self.type_conf = self.load_conf()
 
         main_frame = gtk.VBox(False, 10)
         main_align = gtk.Alignment(0.85,0.85,0.99,0.85) # I don't know why but it works
@@ -58,10 +58,12 @@ class HashTab(nautilus.PropertyPageProvider):
         hash_scrolled_win.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
         hash_scrolled_win.set_size_request(10,240)
 
+        self.hash_entry.connect("activate", self.check_hash)
         entry_label.set_size_request(1,-1)
         
-        start_hash_button.connect("clicked", self.calc_hash, type_conf, file_path)
+        start_hash_button.connect("clicked", self.calc_hash, file_path)
         compare_button.connect("clicked", self.check_hash)
+        settings_button.connect("clicked", self.show_settings)
  
         #Time to pack
         hash_scrolled_win.add_with_viewport(self.hash_tree_view)
@@ -109,7 +111,7 @@ class HashTab(nautilus.PropertyPageProvider):
             conf_file.write('# Line that begins with "#" are comment \n')
             conf_file.write('# Type equal to 0 are disabled, type equal to 1 are enabled, don\'t enable many hash type or hashing will require much time \n \n')
             for hash_type in self.HASH_TYPES:
-                conf_file.write('{0}=0\n'.format(hash_type))            
+                conf_file.write('{0}=False\n'.format(hash_type))            
             conf_file.close()
         except error:
             pass
@@ -120,12 +122,12 @@ class HashTab(nautilus.PropertyPageProvider):
         column = gtk.TreeViewColumn(col_name, cell, text = index)
         return column
 
-    def calc_hash(self, widget, type_conf, file_path):
+    def calc_hash(self, widget, file_path):
         selection = self.hash_tree_view.get_selection()
         model, it = selection.get_selected()
         model.clear()
-        for hash_type in type_conf.keys():
-            if type_conf[hash_type] == '1': # If it's True we should calc this hash
+        for hash_type in self.type_conf.keys():
+            if self.type_conf[hash_type] == '1' or self.type_conf[hash_type] == 'True': # If it's True we should calc this hash
                 if hash_type.lower() in ('md5', 'sha1', 'sha512', 'sha224', 'sha256', 'sha384'): # hashlib function
                     f = open(file_path, 'rb')
                     function = "hashlib.{0}()".format(hash_type.lower())
@@ -147,11 +149,76 @@ class HashTab(nautilus.PropertyPageProvider):
         model, it = selection.get_selected()
         if it:
             if given_hash == model.get_value(it, 1):
-                self.event_box.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse("green"))
-                self.result_label.set_label("Ok")
+                self.event_box.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse("light green"))
+                self.result_label.set_label("Correct hash")
             else:
-                self.event_box.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse("red"))
-                self.result_label.set_label("Ko")
+                self.event_box.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse("indian red"))
+                self.result_label.set_label("Wrong hash")
         else:
-            self.result_label.set_label("Select an hash from the table")
+            self.event_box.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse("light yellow"))
+            self.result_label.set_label('Select an hash from the table \n or click the "Hash it!" button')
+        return
+
+    def show_settings(self, widget):
+        # Show the settings window
+        check_list = {}
+        self.settings_win = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        check_box = gtk.VBox(False, 5)
+        box = gtk.VBox(False, 5)
+        check_box_align = gtk.Alignment(0,0,0,0)
+        buttonbox = gtk.HButtonBox()
+        quit_b = gtk.Button(stock=gtk.STOCK_QUIT)
+        apply_b = gtk.Button(stock=gtk.STOCK_APPLY)
+        buttonbox_align = gtk.Alignment(1,1,0,0)
+
+        self.settings_win.set_size_request(200, -1)
+        self.settings_win.set_title('HashTab settings')
+        self.settings_win.set_position(gtk.WIN_POS_CENTER_ON_PARENT)
+        self.settings_win.set_keep_above(True)
+
+        quit_b.connect("clicked", self.close_set_win)
+        apply_b.connect("clicked", self.apply_conf, check_list)
+
+        buttonbox.pack_start(apply_b)        
+        buttonbox.pack_start(quit_b)
+        buttonbox_align.add(buttonbox)
+
+        for h_t in self.type_conf.keys():
+            check_list[h_t] = gtk.CheckButton(str(h_t))
+            if self.type_conf[h_t] == '1' or self.type_conf[h_t] == 'True':
+                check_list[h_t].set_active(True)
+            check_box.pack_start(check_list[h_t])
+        check_box_align.add(check_box)
+        box.pack_start(check_box_align)
+        box.pack_end(buttonbox_align)
+        self.settings_win.add(box)
+        self.settings_win.show_all()
+       
+        return
+
+    def close_set_win(self, widget):
+        self.settings_win.destroy()
+
+    def apply_conf(self, widget, check_list):
+        conf_file_path = os.path.expanduser("~") +  '/.hash_tab_conf'
+        content_to_write = ""
+        for line in open(conf_file_path, 'r'):
+            if line.startswith('#') or line.find('=') == -1:
+                content_to_write += line
+            else:
+                # Set configration according to check_list
+                hash_type = line[:line.index('=')]
+                content_to_write += hash_type
+                content_to_write += str("=")
+                content_to_write += str(check_list.get(hash_type, gtk.CheckButton).get_active())
+                content_to_write += "\n"
+        f = open(conf_file_path, 'w')
+        for line in content_to_write:
+            f.write(line)
+        f.close()
+
+        # Reload configuration
+        self.type_conf = self.load_conf()
+        self.settings_win.destroy()
+
         return
